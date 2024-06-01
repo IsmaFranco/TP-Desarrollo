2e06642155f6715c8cfb35095231b3c4fa343a9d
chiaracarfagno <164551772+chiaracarfagno@users.noreply.github.com>
2024-06-01 19:32:01 -0300

    Update clothe.entity.ts

--- back-end/src/clothes/entities/clothe.entity.ts
+++ back-end/src/clothes/entities/clothe.entity.ts
@@ -1,1 +1,16 @@
-export class Clothe {}
+export class Clothe {
+   @PrimaryGeneratedColumn('increment')
+    idCl: number;
+
+    @Column({type: 'varchar', length: 200, nullable: true})
+    description: string;
+
+    @Column({type: 'varchar', nullable: false, length:3 })
+    size: string;
+
+    @Column({type: 'varchar', nullable: false, length:200 })
+    typeCl: string;
+
+    @Column ({type: 'integer', nullable: false })
+    stock: integer;
+    }

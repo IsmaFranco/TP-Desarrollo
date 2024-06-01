3d847f5525bd5c71a7430e490b810a09f570b472
chiaracarfagno <164551772+chiaracarfagno@users.noreply.github.com>
2024-06-01 19:17:44 -0300

    Update shipment.entity.ts

--- back-end/src/shipments/entities/shipment.entity.ts
+++ back-end/src/shipments/entities/shipment.entity.ts
@@ -1,1 +1,20 @@
-export class Shipment {}
+export class Shipment { 
+  
+    @PrimaryGeneratedColumn('increment')
+    idSh: number;
+
+    @Column({type: 'real', nullable: false})
+    shipmentCost: real;
+
+    @Column({type: 'timestamp', name:'dateShipmentOut' })
+    dateSh:Date;
+  
+    @Column({type: 'real', nullable: false })
+    actualWeight: real;
+  
+    @Column({type: 'real', nullable: false })
+    volumetricWeight: real: 
+
+
+    /* @Column({type: 'timestamp', name:'updatedPurchase' })
+    updatedPu:Date; cuando cambia el estado */}

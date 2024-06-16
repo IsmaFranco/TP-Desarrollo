1bc369ef0d927d641b2e61f38b8e7d2e18b06a81
ismael <ismaelsfrancotkd@gmail.com>
2024-06-16 18:25:55 -0300

    correccion en app.module

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -6,18 +6,26 @@ import { PurchasesModule } from './purchases/purchases.module';
 import { PriceHistoryModule } from './price-history/price-history.module';
 import { ClothesModule } from './clothes/clothes.module';
 import { ShipmentsModule } from './shipments/shipments.module';
-import { LocationModule } from './location/location.module';
 import { ProvincesModule } from './provinces/provinces.module';
-import { LocationsModule } from './locations/locations.module';
 import { LocalitiesModule } from './localities/localities.module';
-import { PeopleModule } from './people/people.module';
 import { UsersModule } from './users/users.module';
 import { AdminsModule } from './admins/admins.module';
 import { CustomersModule } from './customers/customers.module';
 import { SuppliersModule } from './suppliers/suppliers.module';
 
 @Module({
-  imports: [PurchasesModule, PriceHistoryModule, ClothesModule, ShipmentsModule, LocationModule, ProvincesModule, LocationsModule, LocalitiesModule, PeopleModule, UsersModule, AdminsModule, CustomersModule, SuppliersModule],
+  imports: [
+    PurchasesModule,
+    PriceHistoryModule,
+    ClothesModule,
+    ShipmentsModule,
+    ProvincesModule,
+    LocalitiesModule,
+    UsersModule,
+    AdminsModule,
+    CustomersModule,
+    SuppliersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

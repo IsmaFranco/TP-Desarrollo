4a79ced2dbacff7c325ccb994f9a18bfce174131
ismael <ismaelsfrancotkd@gmail.com>
2024-06-16 18:08:23 -0300

    correccion

--- back-end/src/shipments/entities/shipment.entity.ts
+++ back-end/src/shipments/entities/shipment.entity.ts
@@ -1,20 +1,20 @@
 import { PrimaryGeneratedColumn, Column } from 'typeorm';
-export class Shipment { 
-    @PrimaryGeneratedColumn('increment')
-    idSh: number;
+export class Shipment {
+  @PrimaryGeneratedColumn('increment')
+  idSh: number;
 
-    @Column({type: 'real', nullable: false})
-    shipmentCost: number;
+  @Column({ type: 'real', nullable: false })
+  shipmentCost: number;
 
-    @Column({type: 'timestamp', name:'dateShipmentOut' })
-    dateSh:Date;
+  @Column({ type: 'timestamp', name: 'dateShipmentOut' })
+  dateSh: Date;
 
-    @Column({type: 'real', nullable: false })
-    actualWeight: number;
-    
-    @Column({type: 'real', nullable: false })
-    volumetricWeight: number;
+  @Column({ type: 'real', nullable: false })
+  actualWeight: number;
 
+  @Column({ type: 'real', nullable: false })
+  volumetricWeight: number;
 
-    /* @Column({type: 'timestamp', name:'updatedPurchase' })
-    updatedPu:Date; cuando cambia el estado */}
+  /* @Column({type: 'timestamp', name:'updatedPurchase' })
+  updatedPu:Date; cuando cambia el estado */
+}

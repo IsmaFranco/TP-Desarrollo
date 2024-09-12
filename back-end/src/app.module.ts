ea9f74e64ec909b1147f33d6ca7ad6c1840b4215
ismael <ismaelsfrancotkd@gmail.com>
2024-09-12 20:50:55 -0300

    creo que estan listas las validaciones de create dto

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -26,7 +26,7 @@ import { Locality } from './localities/entities/locality.entity';
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: '127.0.0.1',
-      port: 3307,
+      port: 3306,
       username: 'root',
       password: '',
       database: 'tp-desarrollo',

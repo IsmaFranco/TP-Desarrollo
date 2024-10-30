aee0abf8e32be6bdf383cd43ec986cd39829c404
Ismael <ismaelsfrancotkd@gmail.com>
2024-10-30 11:09:51 -0300

    correciones

--- back-end/src/suppliers/dto/update-supplier.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateSupplierDto } from './create-supplier.dto';
-
-export class UpdateSupplierDto extends PartialType(CreateSupplierDto) {}

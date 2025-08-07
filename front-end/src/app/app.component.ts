9e213596f656f999dbcba1ae06cebf84d850d99c
Gino Zampa <ginojesus2002@gmail.com>
2025-08-07 12:31:00 -0300

    Vista

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { NavComponent } from './pages/nav/nav.component';
+import { isPlatformBrowser } from '@angular/common';
+import { PLATFORM_ID, Inject } from '@angular/core';
+import { TokenService } from './services/token.service';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +13,30 @@ import { NavComponent } from './pages/nav/nav.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
   title = 'front-end';
+
+  constructor(
+    private cdr: ChangeDetectorRef,
+    @Inject(PLATFORM_ID) private platformId: Object,
+    private tokenService: TokenService,
+  ) {}
+
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      // Verificar estado de autenticación después de que el cliente se hidrate
+      setTimeout(() => {
+        this.tokenService.isAutenticated;
+        this.cdr.detectChanges();
+      }, 0);
+    }
+  }
+
+  ngAfterViewInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      // Otra verificación después de que la vista se inicialice
+      this.tokenService.isAutenticated();
+      this.cdr.detectChanges();
+    }
+  }
 }

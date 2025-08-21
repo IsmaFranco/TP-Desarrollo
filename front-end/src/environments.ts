2f24ca6980f7121cb9f212f56e379c397cc7876d
Gino Zampa <ginojesus2002@gmail.com>
2025-08-21 20:33:19 -0300

    url

--- front-end/src/environments.ts
+++ front-end/src/environments.ts
@@ -1,4 +1,3 @@
 export const API_CONFIG = {
-    API_URL: 'http://localhost:3000'
-    //API_URL: 'https://tp-desarrollo-production-de37.up.railway.app'
+    API_URL: 'https://tp-desarrollo-production-de37.up.railway.app'
 };

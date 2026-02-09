877806825d091fe6d73977cbf4e7c5327efd8f3b
Gino Zampa <ginojesus2002@gmail.com>
2026-02-09 20:24:29 -0300

    Verificación de errores

--- back-end/src/payment/payment.service.ts
+++ back-end/src/payment/payment.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { MercadoPagoConfig, Preference } from 'mercadopago';
 import { User } from 'src/users/entities/user.entity';
@@ -9,56 +9,55 @@ export class PaymentService {
   constructor(private readonly configService: ConfigService) { }
 
   async createPayment(items: any[], user: User) {
-    const mercadopago = new MercadoPagoConfig({
-      accessToken: this.configService.get('MP_ACCESS_TOKEN'),
-    });
-    console.log(this.configService.get('MP_ACCESS_TOKEN'));
-    const preference = await new Preference(mercadopago).create({
-      body: {
-        items: [...items.map(item => ({
-          id: item.idCl,
-          title: item.nameCl,
-          quantity: item.quantity,
-          currency_id: 'ARS',
-          unit_price: item.price,
-        })),
-        {
-          id: 'shipping',
-          title: `Envío a ${user.locality.nameLo}`,
-          quantity: 1,
-          currency_id: 'ARS',
-          unit_price: user.locality.cost,
-        }],
-        back_urls: {
-          success: this.configService.get('back_url_success'),
-          failure: this.configService.get('back_url_failure'),
-          pending: this.configService.get('back_url_pending'),
-        },
-        auto_return: 'approved',
-        notification_url: this.configService.get('notification_url'),
-        metadata: {
-          user: {
-            id: user.idUs,
-            idLo: user.locality.idLo,
-            cost: user.locality.cost,
-          },
-          products: items.map(item => ({
-            idCl: item.idCl,
-            name: item.nameCl,
-            price: item.price,
-            quantity: item.quantity
-          })),
-          totalAmount: items.reduce((total, item) => total + (item.price * item.quantity), user.locality.cost)
-        }
-      }
-    });
-
-
     try {
+      const mercadopago = new MercadoPagoConfig({
+        accessToken: this.configService.get('MP_ACCESS_TOKEN'),
+      });
+      const preference = await new Preference(mercadopago).create({
+        body: {
+          items: [...items.map(item => ({
+            id: item.idCl,
+            title: item.nameCl,
+            quantity: item.quantity,
+            currency_id: 'ARS',
+            unit_price: item.price,
+          })),
+          {
+            id: 'shipping',
+            title: `Envío a ${user.locality.nameLo}`,
+            quantity: 1,
+            currency_id: 'ARS',
+            unit_price: user.locality.cost,
+          }],
+          back_urls: {
+            success: this.configService.get('back_url_success'),
+            failure: this.configService.get('back_url_failure'),
+            pending: this.configService.get('back_url_pending'),
+          },
+          auto_return: 'approved',
+          notification_url: this.configService.get('notification_url'),
+          metadata: {
+            user: {
+              id: user.idUs,
+              idLo: user.locality.idLo,
+              cost: user.locality.cost,
+            },
+            products: items.map(item => ({
+              idCl: item.idCl,
+              name: item.nameCl,
+              price: item.price,
+              quantity: item.quantity
+            })),
+            totalAmount: items.reduce((total, item) => total + (item.price * item.quantity), user.locality.cost)
+          }
+        }
+      });
+
       console.log('Preferencia creada:', preference);
       return { init_point: preference.init_point };
     } catch (error) {
-      throw new Error(`Error al crear el pago: ${error.message}`);
+      console.error('Error al crear el pago:', error.message || error);
+      throw new InternalServerErrorException(`Error al crear el pago: ${error.message}`);
     }
   }
 }
